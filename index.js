424950adee5d9410860dabe7a8714336dea9e5ca
bajraroshan <bajraroshan@gmail.com>
2021-11-30 01:24:05 +1030

    Complete Script for the app

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 //DEPENDENCIES
 const inquirer = require("inquirer");
-const { printTable } = require('console-table-printer');
+const { printTable } = require("console-table-printer");
+const db = require("./config/connection");
+var figlet = require("figlet");
 
-const figlet = require("figlet");
+
 figlet("Employee \n Management \n System", function (err, data) {
   if (err) {
     console.log("Something went wrong...");
@@ -10,12 +12,30 @@ figlet("Employee \n Management \n System", function (err, data) {
     return;
   }
   console.log(data);
-  init();
+  userPrompt();
 });
 
-const db = require("./config/connection");
+const confirmation = () => {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "again",
+        message: `Would you like to continue?`,
+        choices: [`Yes`, `No`],
+      },
+    ])
+    .then(({ again }) => {
+      if (again === `Yes`) {
+        console.clear();
+        userPrompt();
+      } else {
+        exit();
+      }
+    });
+};
 
-const init = () => {
+const userPrompt = () => {
   inquirer
     .prompt([
       {
@@ -23,127 +43,93 @@ const init = () => {
         name: "userSelect",
         message: `What would you like to do?`,
         choices: [
-          `View All Employees`,
-          `Add Employee`,
-          `Remove Employee`,
-          `Update Employee role`,
-          `Update Employee Manager`,
-          `View All Employees By Department`,
-          `View All Department`,
-          `Add Department`,
-          `Remove Department`,
-          `View All Role`,
-          `Add Role`,
-          `Remove Role`,
-          `View All Employees By Manager`,
-          `View Total Utilized Budget By Department`,
-          `EXIT`,
+          "View All Department",
+          "View All Role",
+          "View All Employees",
+          "Add a Department",
+          "Add a Role",
+          "Add an Employee",
+          "Update an Employee Role",
+          "Update Employee Manager",
+          "View All Employees By Manager",
+          "View All Employees By Department",
+          "Delete Department",
+          "Delete Role",
+          "Delete Employee",
+          "View Total Utilized Budget By Department",
+          "EXIT",
         ],
       },
     ])
     .then(({ userSelect }) => {
-      if (userSelect === `View All Employees`) {
-        viewAllEmployees();
+      if (userSelect === "View All Department") {
+        viewDepartment();
       }
-      if (userSelect === `View All Employees By Department`) {
-        viewEmpByDept();
+      if (userSelect === "View All Role") {
+        viewRole();
       }
-      if (userSelect === `View All Employees By Manager`) {
-        viewEmpByMgr();
+      if (userSelect === "View All Employees") {
+        viewEmployees();
       }
-      if (userSelect === `Add Employee`) {
-        addEmp();
+      if (userSelect === "Add a Department") {
+        addDepartment();
       }
-      if (userSelect === `Remove Employee`) {
-        removeEmp();
-      }
-      if (userSelect === `Update Employee role`) {
-        updateEmp();
-      }
-      if (userSelect === `Update Employee Manager`) {
-        updateEmpMgr();
-      }
-      if (userSelect === `Add Department`) {
-        addDept();
-      }
-      if (userSelect === `Remove Department`) {
-        removeDept();
-      }
-      if (userSelect === `Add Role`) {
+      if (userSelect === "Add a Role") {
         addRole();
       }
-      if (userSelect === `Remove Role`) {
-        removeRole();
+      if (userSelect === "Add an Employee") {
+        addEmployee();
       }
-      if (userSelect === `View All Department`) {
-        viewDept();
+      if (userSelect === "Update an Employee Role") {
+        updateEmployee();
       }
-      if (userSelect === `View All Role`) {
-        viewRoles();
+      if (userSelect === "Update Employee Manager") {
+        updateEmployeeManager();
       }
-      if (userSelect === `View Total Utilized Budget By Department`) {
+      if (userSelect === "View All Employees By Manager") {
+        viewEmployeeByManager();
+      }
+      if (userSelect === "View All Employees By Department") {
+        viewEmployeebyDepartment();
+      }
+      if (userSelect === "Delete Department") {
+        deleteDepartment();
+      }
+      if (userSelect === "Delete Role") {
+        deleteRole();
+      }
+      if (userSelect === "Delete Employee") {
+        deleteEmployee();
+      }
+      if (userSelect === "View Total Utilized Budget By Department") {
         viewBudget();
       }
-      if (userSelect === `EXIT`) {
+      if (userSelect === "EXIT") {
         exit();
       }
     });
 };
 
-viewAllEmployees = () =>
+viewEmployees = () =>
   db
     .promise()
     .query(
-      `SELECT employee.id,
- CONCAT (employee.first_name, " ", employee.last_name) AS employee,
- role.title,
- department.name AS department ,
- role.salary,
- CONCAT (manager.first_name, " ", manager.last_name) AS manager FROM employee
- LEFT JOIN role ON employee.role_id = role.id
- LEFT JOIN department ON role.department_id = department.id
- LEFT JOIN employee manager ON employee.manager_id = manager.id;`
+      `SELECT employee.id AS ID,
+  CONCAT (employee.first_name, " ", employee.last_name) AS Employee,
+  role.title AS Title,
+  department.name AS Department ,
+  role.salary AS Salary,
+  CONCAT (manager.first_name, " ", manager.last_name) AS Manager FROM employee
+  LEFT JOIN role ON employee.role_id = role.id
+  LEFT JOIN department ON role.department_id = department.id
+  LEFT JOIN employee manager ON employee.manager_id = manager.id;`
     )
     .then(([rows, fields]) => {
       printTable(rows);
-      init();
+      confirmation();
     });
 
-viewEmpByDept = () =>
-  db
-    .promise()
-    .query(
-      `SELECT department.name AS department,
- CONCAT (employee.first_name, " ", employee.last_name) as employee
- FROM employee 
- LEFT JOIN role ON employee.role_id = role.id 
- LEFT JOIN department ON role.department_id = department.id
- ORDER by department DESC`
-    )
-    .then(([rows, fields]) => {
-      printTable(rows);
-      init();
-    });
-
-viewEmpByMgr = () =>
-  db
-    .promise()
-    .query(
-      `SELECT CONCAT (manager.first_name, " ", manager.last_name) AS manager,
- CONCAT (employee.first_name, " ", employee.last_name) as employee,
- department.name AS department
- FROM employee 
- LEFT JOIN role ON employee.role_id = role.id 
- LEFT JOIN department ON role.department_id = department.id
- LEFT JOIN employee manager ON employee.manager_id = manager.id
- ORDER by manager DESC;`
-    )
-    .then(([rows, fields]) => {
-      printTable(rows);
-      init();
-    });
-
-addEmp = () => {
+addEmployee = () => {
   inquirer
     .prompt([
       {
@@ -154,7 +140,7 @@ addEmp = () => {
           if (checkFirst) {
             return true;
           } else {
-            console.log("Please enter a first name".red);
+            console.log("Please enter a first name");
             return false;
           }
         },
@@ -167,7 +153,7 @@ addEmp = () => {
           if (checkLast) {
             return true;
           } else {
-            console.log("Please enter a last name".red);
+            console.log("Please enter a last name");
             return false;
           }
         },
@@ -179,7 +165,7 @@ addEmp = () => {
       db.promise()
         .query(`SELECT role.id, role.title FROM role`)
         .then(([rows, fields]) => {
-          const roles = rows.map(({ id, title }) => ({
+          const role = rows.map(({ id, title }) => ({
             name: title,
             value: id,
           }));
@@ -189,7 +175,7 @@ addEmp = () => {
                 type: "list",
                 name: "role",
                 message: `What is the employee's role?`,
-                choices: roles,
+                choices: role,
               },
             ])
             .then((answer) => {
@@ -215,10 +201,10 @@ addEmp = () => {
                     .then((answer) => {
                       employee.push(answer.manager);
                       console.log("Employee has been added!");
-                      init();
+                      confirmation();
                       db.query(
                         `INSERT INTO employee (first_name, last_name, role_id, manager_id)
-                 VALUES (?, ?, ? ,?)`,
+                    VALUES (?, ?, ? ,?)`,
                         employee,
                         (err, result) => {
                           if (err) {
@@ -233,7 +219,60 @@ addEmp = () => {
     });
 };
 
-removeEmp = () => {
+updateEmployee = () => {
+  db.query(`SELECT * FROM employee`, (err, data) => {
+    if (err) throw err;
+
+    const employees = data.map(({ id, first_name, last_name }) => ({
+      name: first_name + " " + last_name,
+      value: id,
+    }));
+
+    inquirer
+      .prompt([
+        {
+          type: "list",
+          name: "name",
+          message: "Which employee would you like to update?",
+          choices: employees,
+        },
+      ])
+      .then((answer) => {
+        db.query(`SELECT * FROM role`, (err, data) => {
+          if (err) throw err;
+          const employee = answer.name;
+          const role = data.map(({ id, title }) => ({
+            name: title,
+            value: id,
+          }));
+          inquirer
+            .prompt([
+              {
+                type: "list",
+                name: "role",
+                message: `What is the employee's new role?`,
+                choices: role,
+              },
+            ])
+            .then((answer) => {
+              const role = answer.role;
+              const arr = [role, employee];
+              db.query(
+                `UPDATE employee SET role_id = ? WHERE id = ?`,
+                arr,
+                (err, result) => {
+                  if (err) throw err;
+                  console.log("Employee has been updated!");
+                  confirmation();
+                }
+              );
+            });
+        });
+      });
+  });
+};
+
+deleteEmployee = () => {
   db.query(`SELECT * FROM employee`, (err, data) => {
     if (err) throw err;
 
@@ -261,67 +300,46 @@ removeEmp = () => {
             if (err) throw err;
             console.log("Successfully Deleted!");
 
-            init();
+            confirmation();
           }
         );
       });
   });
 };
 
-updateEmp = () => {
-  db.query(`SELECT * FROM employee`, (err, data) => {
-    if (err) throw err;
+viewEmployeebyDepartment = () =>
+  db
+    .promise()
+    .query(
+      `SELECT department.name AS Department,
+  CONCAT (employee.first_name, " ", employee.last_name) as Employee
+  FROM employee 
+  LEFT JOIN role ON employee.role_id = role.id 
+  LEFT JOIN department ON role.department_id = department.id;`
+    )
+    .then(([rows, fields]) => {
+      printTable(rows);
+      confirmation();
+    });
 
-    const employees = data.map(({ id, first_name, last_name }) => ({
-      name: first_name + " " + last_name,
-      value: id,
-    }));
+viewEmployeeByManager = () =>
+  db
+    .promise()
+    .query(
+      `SELECT CONCAT (manager.first_name, " ", manager.last_name) AS Manager,
+  CONCAT (employee.first_name, " ", employee.last_name) as Employee,
+  department.name AS department
+  FROM employee 
+  LEFT JOIN role ON employee.role_id = role.id 
+  LEFT JOIN department ON role.department_id = department.id
+  LEFT JOIN employee manager ON employee.manager_id = manager.id;`
+    )
+    .then(([rows, fields]) => {
+      printTable(rows);
+      confirmation();
+    });
 
-    inquirer
-      .prompt([
-        {
-          type: "list",
-          name: "name",
-          message: "Which employee would you like to update?",
-          choices: employees,
-        },
-      ])
-      .then((answer) => {
-        db.query(`SELECT * FROM role`, (err, data) => {
-          if (err) throw err;
-          const employee = answer.name;
-          const roles = data.map(({ id, title }) => ({
-            name: title,
-            value: id,
-          }));
-          inquirer
-            .prompt([
-              {
-                type: "list",
-                name: "role",
-                message: `What is the employee's new role?`,
-                choices: roles,
-              },
-            ])
-            .then((answer) => {
-              const role = answer.role;
-              const arr = [role, employee];
-              db.query(
-                `UPDATE employee SET role_id = ? WHERE id = ?`,
-                arr,
-                (err, result) => {
-                  if (err) throw err;
-                  console.log("Employee has been updated!");
-                  init();
-                }
-              );
-            });
-        });
-      });
-  });
-};
-
-updateEmpMgr = () => {
+updateEmployeeManager = () => {
   db.query(`SELECT * FROM employee`, (err, data) => {
     if (err) throw err;
 
@@ -365,7 +383,7 @@ updateEmpMgr = () => {
               (err, result) => {
                 if (err) throw err;
                 console.log("Employee has been updated!");
-                init();
+                confirmation();
               }
             );
           });
@@ -373,7 +391,33 @@ updateEmpMgr = () => {
   });
 };
 
-addDept = () => {
+viewBudget = () => {
+  db.query(
+    `SELECT department.name AS Department,
+  SUM(role.salary) as Budget
+  FROM department
+  LEFT JOIN role ON department_id = department.id
+  GROUP BY department.id;`,
+    (err, data) => {
+      if (err) throw err;
+      printTable(data);
+      confirmation();
+    }
+  );
+};
+
+viewDepartment = () => {
+  db.query(
+    `SELECT department.name AS Department FROM department`,
+    (err, data) => {
+      if (err) throw err;
+      printTable(data);
+      confirmation();
+    }
+  );
+};
+
+addDepartment = () => {
   inquirer
     .prompt([
       {
@@ -389,10 +433,54 @@ addDept = () => {
         (err, result) => {
           if (err) throw err;
           console.log("Department has been added!");
-          init();
+          confirmation();
         }
       );
     });
+};
+
+deleteDepartment = () => {
+  db.query(`SELECT * FROM department`, (err, data) => {
+    if (err) throw err;
+
+    const department = data.map(({ id, name }) => ({ name: name, value: id }));
+
+    inquirer
+      .prompt([
+        {
+          type: "list",
+          name: "name",
+          message: "Which department would you like to delete?",
+          choices: department,
+        },
+      ])
+      .then((answer) => {
+        db.query(
+          `DELETE FROM department WHERE id = ?`,
+          answer.name,
+          (err, result) => {
+            if (err) throw err;
+            console.log("Successfully Deleted!");
+
+            confirmation();
+          }
+        );
+      });
+  });
+};
+
+viewRole = () => {
+  db.query(
+    `SELECT role.title AS Title,
+      role.salary AS Salary,
+      department.name AS Department
+      FROM role LEFT JOIN department ON role.department_id = department.id;`,
+    (err, data) => {
+      if (err) throw err;
+      printTable(data);
+      confirmation();
+    }
+  );
 };
 
 addRole = () => {
@@ -443,7 +531,7 @@ addRole = () => {
                   (err, result) => {
                     if (err) throw err;
                     console.log("Role has been added!");
-                    init();
+                    confirmation();
                   }
                 );
               });
@@ -452,37 +540,7 @@ addRole = () => {
     });
 };
 
-removeDept = () => {
-  db.query(`SELECT * FROM department`, (err, data) => {
-    if (err) throw err;
-
-    const department = data.map(({ id, name }) => ({ name: name, value: id }));
-
-    inquirer
-      .prompt([
-        {
-          type: "list",
-          name: "name",
-          message: "Which department would you like to delete?",
-          choices: department,
-        },
-      ])
-      .then((answer) => {
-        db.query(
-          `DELETE FROM department WHERE id = ?`,
-          answer.name,
-          (err, result) => {
-            if (err) throw err;
-            console.log("Successfully Deleted!");
-
-            init();
-          }
-        );
-      });
-  });
-};
-
-removeRole = () => {
+deleteRole = () => {
   db.query(`SELECT * FROM role`, (err, data) => {
     if (err) throw err;
 
@@ -505,51 +563,11 @@ removeRole = () => {
             if (err) throw err;
             console.log("Successfully Deleted!");
 
-            init();
+            confirmation();
           }
         );
       });
   });
-};
-
-viewDept = () => {
-  db.query(
-    `SELECT department.name AS department FROM department`,
-    (err, data) => {
-      if (err) throw err;
-      printTable(data);
-      init();
-    }
-  );
-};
-
-viewRoles = () => {
-  db.query(
-    `SELECT role.title,
-     role.salary,
-     department.name AS department
-     FROM role LEFT JOIN department ON role.department_id = department.id;`,
-    (err, data) => {
-      if (err) throw err;
-      printTable(data);
-      init();
-    }
-  );
-};
-
-viewBudget = () => {
-  db.query(
-    `SELECT department.name AS department,
- SUM(role.salary) as budget
- FROM department
- LEFT JOIN role ON department_id = department.id
- GROUP BY department.id;`,
-    (err, data) => {
-      if (err) throw err;
-      printTable(data);
-      init();
-    }
-  );
 };
 
 exit = () => {
