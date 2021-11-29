0cdee9f675bc33b80014a204e6a126121c7327c9
bajraroshan <bajraroshan@gmail.com>
2021-11-29 21:30:05 +1030

    Added question for prompt

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,15 @@
-const Sequelize = require('sequelize');
+const mysql = require("mysql2");
 require('dotenv').config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306,
-  }
-);
+const connection = mysql.createConnection({
+  host: 'localhost',
+  port: 3306,
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD
+},
+(err) => {
+    console.log(err)
+});
 
-module.exports = sequelize;
+module.exports = connection;
--- index.js
+++ index.js
@@ -1,12 +1,74 @@
 //DEPENDENCIES
-const mysql = require("mysql2");
 const inquirer = require("inquirer");
-const express = require('express');
-const connection = require('./config/connection');
+const connection = require("./config/connection");
+const cTable = require("console.table");
 
-const app = express();
-const PORT = process.env.PORT || 3001;
+const departments = require("./lib/departments");
+const roles = require("./lib/roles");
+const employees = require("./lib/employees");
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+const init = async () => {
+  await inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "input",
+        message: "What would you like to do?",
+        choices: [
+          "View all departments",
+          "View all roles",
+          "View all employees",
+          "Add a department",
+          "Add a role",
+          "Add an employee",
+          "Update an employee role",
+          "Quit",
+        ],
+      },
+    ])
+    .then((data) => {
+    //   console.log("You entered " + data.input);
 
+      if (data.input === "View all departments") {
+        console.log("View all department");
+        departments.viewDepartment();
+        init();
+      } else if (data.input === "View all roles") {
+        console.log("View all roles");
+        roles.viewRole();
+        init();
+      } else if (data.input === "View all employees") {
+        console.log("View all employees");
+        employees.viewEmployees();
+        init();
+      } else if (data.input === "Add a department") {
+        const order = async () => {
+          console.log("Add a department");
+          await departments.addDepartment();
+          await init();
+        };
+        order();
+      } else if (data.input === "Add a role") {
+        const order = async () => {
+          console.log("Add a role");
+          await roles.addRole();
+          await init();
+        };
+        order();
+      } else if (data.input === "Add an employee") {
+        const order = async () => {
+          console.log("Add an employee");
+          await employees.addEmployee();
+          await init();
+        };
+        order();
+      } else if (data.input === "Quit") {
+        return;
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+init();
